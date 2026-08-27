9b89ccfe416b17ec91d4868f852a663a0000f62b
Softlink <arielgiovanni90@gmail.com>
2026-08-26 19:54:40 -0600

    apoyo a odir desarrolle ejemplo10

---
+++ Ejemplos/ejemplo10/Biblioteca.java
@@ -0,0 +1,20 @@
+package Ejemplos.ejemplo10;
+
+public class Biblioteca {
+    private Libro[] coleccion = new Libro[3];
+ private int cantidad = 0;
+ public boolean agregarLibro(Libro libro) {
+ if (cantidad < coleccion.length) {
+ coleccion[cantidad] = libro;
+ cantidad++;
+ return true;
+ }
+ return false;
+ }
+ public void listarLibros() {
+ for (int i = 0; i < cantidad; i++) {
+ System.out.println((i + 1) + ". " + coleccion[i].titulo);
+ }
+ }
+
+}
---
+++ Ejemplos/ejemplo10/Libro.java
@@ -0,0 +1,9 @@
+package Ejemplos.ejemplo10;
+
+public class Libro {
+    public String titulo;
+    public Libro(String titulo) {
+    this.titulo = titulo;
+    }
+
+}
